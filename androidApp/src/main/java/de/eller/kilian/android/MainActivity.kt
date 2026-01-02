07110739dd6152898fbc89695d9dd04d2952d35f
Nailik <13292441+Nailik@users.noreply.github.com>
2026-01-02 16:09:33 +0100

    fix android edge to edge settings (status bar icon colors)

--- androidApp/src/main/java/de/eller/kilian/android/MainActivity.kt
+++ androidApp/src/main/java/de/eller/kilian/android/MainActivity.kt
@@ -3,11 +3,14 @@ package de.eller.kilian.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import de.eller.kilian.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         setContent {
             App()
